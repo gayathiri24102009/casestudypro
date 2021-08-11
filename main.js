1dbc555f3935d2df7ea6941c6ef7d4fadc36728e
gayathiri24102009 <78957848+gayathiri24102009@users.noreply.github.com>
2021-08-11 23:25:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+Webcam.set({
+    width:310,
+    height:300,
+    image_format :'png',
+    png_quality:90,
+
+    contraint: {
+        facingMOde: "environment"
+    }
+});
+
+  camera=document.getElementById("camera");
+
+  Webcam .attach(camera);
